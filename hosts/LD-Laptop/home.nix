2b4dbce29fff6cd7e7b5a5bc5ff1093bbb195725
LDprg <lukas_4dr@gmx.at>
2023-09-07 20:38:40 +0200

    move some default apps

--- hosts/LD-Laptop/home.nix
+++ hosts/LD-Laptop/home.nix
@@ -2,17 +2,11 @@
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
 
 { inputs, outputs, user, host, lib, config, pkgs, ... }: {
-  imports = [
-    ../../modules/home-manager/default-apps.nix
-  ];
+  imports = [ ../../modules/home-manager/default-apps.nix ];
 
   home.packages = with pkgs; [
-    firefox
-    gparted
     kate
     thunderbird
-    vscode
-    nixfmt
     helvum
     easyeffects
     protonup-qt
--- modules/home-manager/default-apps.nix
+++ modules/home-manager/default-apps.nix
@@ -1,5 +1,3 @@
 { inputs, outputs, user, host, lib, config, pkgs, ... }: {
-  home.packages = with pkgs; [
-    discord
-  ];
+  home.packages = with pkgs; [ firefox discord gparted vscode nixfmt ];
 }
