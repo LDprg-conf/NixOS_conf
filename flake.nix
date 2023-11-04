7ebc572f9701754a159f5350ee2f9e30e81eb99b
LDprg <lukas_4dr@gmx.at>
2023-11-04 12:36:01 +0100

    move to kernel 6.6

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "A Nixos config";
 
   nixConfig = {
-    extra-trusted-substituters =
-      [ "https://nix-community.cachix.org" ];
+    extra-trusted-substituters = [ "https://nix-community.cachix.org" ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
