d0242b88679674d9d59db984be2756071cf790ce
LDprg <lukas_4dr@gmx.at>
2023-11-04 13:43:36 +0100

    fix gdm monitor

--- modules/nixos/plasma.nix
+++ modules/nixos/plasma.nix
@@ -6,6 +6,49 @@
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
 
+  systemd.tmpfiles.rules = [
+    "L+ /run/gdm/.config/monitors.xml - - - - ${
+      pkgs.writeText "gdm-monitors.xml" ''
+        <monitors version="2">
+          <configuration>
+            <logicalmonitor>
+              <x>0</x>
+              <y>0</y>
+              <scale>1</scale>
+              <primary>yes</primary>
+              <monitor>
+                <monitorspec>
+                  <connector>HDMI-1</connector>
+                  <vendor>BNQ</vendor>
+                  <product>BenQ LCD</product>
+                  <serial>A7G03989019</serial>
+                </monitorspec>
+                <mode>
+                  <width>1920</width>
+                  <height>1080</height>
+                  <rate>60.000</rate>
+                </mode>
+              </monitor>
+              <monitor>
+                <monitorspec>
+                  <connector>eDP-1</connector>
+                  <vendor>CMN</vendor>
+                  <product>0x1521</product>
+                  <serial>0x00000000</serial>
+                </monitorspec>
+                <mode>
+                  <width>1920</width>
+                  <height>1080</height>
+                  <rate>144.003</rate>
+                </mode>
+              </monitor>
+            </logicalmonitor>
+          </configuration>
+        </monitors>
+      ''
+    }"
+  ];
+
   services.xserver.desktopManager.xterm.enable = false;
 
   services.xserver.desktopManager.plasma5.enable = true;
