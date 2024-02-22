c13dd19f032ca7fb484dcfac48654fd605266f77
LDprg <lukas_4dr@gmx.at>
2024-02-22 18:12:35 +0100

    extend fish prompt, fix vscode problems

--- modules/home-manager/default-apps.nix
+++ modules/home-manager/default-apps.nix
@@ -2,4 +2,11 @@
 }: {
   home.packages = with pkgs; [ discord gparted nixfmt glxinfo vlc ];
   imports = [ ./vscode.nix ];
+
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      nix-your-shell fish | source
+    '';
+  };
 }
--- modules/nixos/fish.nix
+++ modules/nixos/fish.nix
@@ -1,23 +1,21 @@
 { inputs, outputs, self, user, host, lib, config, pkgs, nix-your-shell, ... }: {
   environment.systemPackages = with pkgs; [
-    fishPlugins.bass
+    #fishPlugins.bass
     #fishPlugins.done
     fishPlugins.fzf-fish
-    fishPlugins.forgit
-    fishPlugins.hydro
     fzf
     fishPlugins.grc
     grc
+    fishPlugins.forgit
+    fishPlugins.tide
+    fishPlugins.sponge
+    fishPlugins.autopair
   ];
 
   programs.fish = {
     enable = true;
     promptInit = ''
       nix-your-shell fish | source
-      set --global hydro_color_pwd $fish_color_operator
-      set --global hydro_color_prompt $fish_color_operator
-      set --global hydro_color_git $fish_color_user
-      set --global hydro_color_duration $fish_color_normal
     '';
   };
 }
