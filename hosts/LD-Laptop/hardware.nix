9c45302ad6e80fb5f753634ab657cd057ce8706a
LDprg <lukas_4dr@gmx.at>
2024-03-29 21:10:39 +0100

    replace auto-cpufreq with tlp

--- hosts/LD-Laptop/hardware.nix
+++ hosts/LD-Laptop/hardware.nix
@@ -129,20 +129,41 @@
       xserver.videoDrivers = [ "nvidia" ];
 
       power-profiles-daemon.enable = false;
-      tlp.enable = false;
-      auto-cpufreq.enable = true;
 
-      # pstate driver is managing the turbo boost so no need to set it in auto-cpufreq
-      auto-cpufreq.settings = {
-        battery = {
-          governor = "powersave";
-          energy_performance_preference =
-            "balance_power"; # Powersaving without too less performance (power will lock cpu freq at 400 MHz)
-        };
-        charger = {
-          governor = "performance";
-          energy_performance_preference =
-            "performance"; # Full performance on AC
+      tlp = {
+        enable = true;
+        settings = {
+          CPU_SCALING_GOVERNOR_ON_AC = "performance";
+          CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+          CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+          CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+          CPU_SCALING_MIN_FREQ_ON_AC = 400000;
+          CPU_SCALING_MAX_FREQ_ON_AC = 4463000;
+          CPU_SCALING_MIN_FREQ_ON_BAT = 400000;
+          CPU_SCALING_MAX_FREQ_ON_BAT = 1000000;
+
+          CPU_BOOST_ON_AC = 1;
+          CPU_BOOST_ON_BAT = 0;
+
+          CPU_MIN_PERF_ON_AC = 0;
+          CPU_MAX_PERF_ON_AC = 100;
+          CPU_MIN_PERF_ON_BAT = 0;
+          CPU_MAX_PERF_ON_BAT = 30;
+
+          USB_EXCLUDE_BTUSB = 1;
+
+          PCIE_ASPM_ON_AC = "performance";
+          PCIE_ASPM_ON_BAT = "powersave";
+
+          RADEON_DPM_STATE_ON_AC = "performance";
+          RADEON_DPM_STATE_ON_BAT = "battery";
+
+          RESTORE_THRESHOLDS_ON_BAT = 1;
+
+          START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+          STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
         };
       };
 
