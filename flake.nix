7e0a4599c17c8c48d9e2ef60e6330f558fc70fac
LDprg <lukas_4dr@gmx.at>
2024-04-28 11:36:59 +0200

    fix trusted substituters

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   nixConfig = {
     extra-trusted-substituters =
-      [ "https://nix-community.cachix.org" "https://nyx.chaotic.cx" ];
+      [ "https://nix-community.cachix.org" "https://nyx.chaotic.cx/" ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8= chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+      "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
     ];
   };
 
